e8fb53084388dbdd0b4a61a7d2c83a5428d8c785
Matthieu Koskas <matthieu.koskas@obs-mip.fr>
2019-12-06 13:42:13 +0100

    hello working, removed eslint

---
+++ src/components/main-page.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+    {{ dataTest }}
+</div>
+    
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            dataTest: "Hello"
+        }
+    }
+}
+</script>
+
+<style scoped>
+*{
+    box-sizing: border-box;
+    background-color: rgb(104, 61, 112);
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,12 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    resources: [
+      {
+        title: "A great title!",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vel accumsan metus. Nam mollis pretium. "
+      }
+    ]
   },
   mutations: {
   },
